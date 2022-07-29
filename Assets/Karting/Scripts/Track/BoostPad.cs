e2b92a5402e4cbb1a373c175e4a9d31f11f9726a
rallanmorris <rallanmorris@gmail.com>
2022-07-29 14:43:51 -0400

    Added more of the loop and scripted a speed pad TODO: Setup Drifting boost, Script Cannons, Finish loop

---
+++ Assets/Karting/Scripts/Track/BoostPad.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using KartGame.KartSystems;
+
+public class BoostPad : MonoBehaviour
+{
+	public int boostAmount = 3;
+	public void OnTriggerEnter(Collider other)
+	{
+		if(other.GetComponentInParent<ArcadeKart>() != null)
+		{
+			other.GetComponentInParent<ArcadeKart>().Boost(boostAmount);
+		}
+	}
+}
